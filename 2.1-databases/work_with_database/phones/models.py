3cf23ad38d2dc1ca4044f111e52a71c941a2bebd
VolshVs <89104047740@mail.ru>
2024-02-16 21:24:55 +0300

    Update files

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,22 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=55, unique=True, verbose_name='Имя (name)')
+    price = models.PositiveIntegerField(default=0, verbose_name='Цена (price)')
+    image = models.ImageField(verbose_name='Фоточки')
+    release_date = models.DateField(default=None, blank=True, verbose_name='Дата (release_date)')
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=55, unique=True, db_index=True, verbose_name="slug")
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}, {self.price}, {self.image}, {self.release_date}, {self.lte_exists}, {self.slug}'
+
+    # def get_absolute_url(self):
+    #     return reverse('product', kwargs={'slug': self.slug})
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +9,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_dict = {
+        "name": 'name',
+        "min_price": 'price',
+        "max_price": "-price"
+    }
+    phones = Phone.objects.all()
+    page_status = request.GET.get('sort', '')
+    if page_status:
+        phones = phones.order_by(sort_dict[page_status])
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
